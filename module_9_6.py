9592534364e3f0a671e76cbb0b9fe50d3f7a51c6
Григорий <124524205+GriHan-909@users.noreply.github.com>
2024-07-22 19:18:44 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,13 @@
+def all_variants(text):
+    count = 1
+    while count<=len(text):
+        for i in range(len(text)):
+            if (i+count) > len(text):
+                continue            
+            yield text[i:i + count]
+        count+=1
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
