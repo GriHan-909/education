7705098a8f157e85eae13ec89880bc6ee4ff3802
Григорий <124524205+GriHan-909@users.noreply.github.com>
2024-09-03 20:38:17 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -1,21 +1,18 @@
-
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-api = '**********************************************'
+api = '***'
 bot = Bot(token=api)
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    user = message.from_user
+async def start(message):
     print('Привет! Я бот помогающий твоему здоровью.')
-    await message.answer(text=f'Привет {user.username}! Я бот помогающий твоему здоровью.')
+
 
 @dp.message_handler()
-async def all_massages(message: types.Message):    
+async def all_massages(message):
     print('Введите команду /start, чтобы начать общение.')
-    await message.answer(text='Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
