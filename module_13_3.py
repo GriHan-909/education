07653010150d7043f612006b464c8ef93676f959
Григорий <124524205+GriHan-909@users.noreply.github.com>
2024-09-03 20:38:53 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = '***'
+bot = Bot(token=api)
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    user = message.from_user
+    await message.answer(text=f'Привет {user.username}! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message: types.Message):
+    await message.answer(text='Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
