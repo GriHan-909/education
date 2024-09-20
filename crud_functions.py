a7e2f0ce9674180423f03064b7c16033d55ac5e2
Григорий <124524205+GriHan-909@users.noreply.github.com>
2024-09-20 03:01:05 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect('database3.db')
 cursor = connection.cursor()
 
 
@@ -14,6 +14,15 @@ async def initiate_db():
         description TEXT,
         price INTEGER NOT NULL);
 ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL);
+''')
+
     for i in range(1, 5):
         cursor.execute('INSERT INTO Products (title, description, price) VALUES(?, ?, ?)', (f"Продукт {i}", f"Описание {i}", i*100))
     connection.commit()
@@ -24,5 +33,17 @@ async def get_all_products():
     result = cursor.fetchall()
     return result
 
+async def add_user(username, email, age):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)', (username, email, age, 1000))
+    connection.commit()
+
+async def is_included(username):
+    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    result = cursor.fetchone()
+
+    if result is None:
+        return False
+    else:
+        return True
 
 # connection.close()
---
+++ module_14_5.py
@@ -0,0 +1,141 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from crud_functions import initiate_db, get_all_products, add_user, is_included
+
+api = '7339132684:AAHPHn1uN3PPthl9prsl6wHnRG2KaGPl1Xc'
+bot = Bot(token=api)
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
+
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+button = KeyboardButton(text = 'Рассчитать')
+button_2 = KeyboardButton(text = 'Информация')
+button_3 = KeyboardButton(text = "Купить")
+button_4 = KeyboardButton(text='Регистрация')
+kb.add(button, button_2, button_3, button_4)
+
+inl_kb = InlineKeyboardMarkup()
+inl_button = InlineKeyboardButton(text = 'Рассчитать норму калорий', callback_data='calories')
+inl_button_2 = InlineKeyboardButton(text= 'Формулы расчета', callback_data='formulas')
+inl_kb.add(inl_button, inl_button_2)
+
+inl_kb_buy = InlineKeyboardMarkup(row_width= 4)
+inl_buy = InlineKeyboardButton(text = 'Product 1', callback_data='product_buying')
+inl_buy2 = InlineKeyboardButton(text = 'Product 2', callback_data='product_buying')
+inl_buy3 = InlineKeyboardButton(text = 'Product 3', callback_data='product_buying')
+inl_buy4 = InlineKeyboardButton(text = 'Product 4', callback_data='product_buying')
+inl_kb_buy.add(inl_buy, inl_buy2,inl_buy3,inl_buy4)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    user = message.from_user
+    await initiate_db()
+    await message.answer(text=f'Привет {user.username}! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+@dp.message_handler(text ='Рассчитать')
+async def main_menu(message: types.Message):
+    await message.answer(text='Выберите опцию:', reply_markup=inl_kb)
+
+
+@dp.callback_query_handler(text ='calories')
+async def set_age(call: types.CallbackQuery):
+    await call.answer()
+    await call.message.answer(text='Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call: types.CallbackQuery):
+    await call.answer()
+    await call.message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer(text='Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer(text='Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    result = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age'])
+    await state.finish()
+    await message.answer(f'Ваша норма каллорий: {result}')
+
+
+@dp.message_handler(text = 'Купить')
+async def get_buying_list(message: types.Message):
+    image = ['1.png', '2.png', '3.png', '4.png']
+    value_db = await get_all_products()
+    for i, value in enumerate(value_db):
+        await message.answer(f'Название: {value[0]} | Описание: {value[1]} | Цена: {value[2]}')
+        with open(f'image/{image[i]}', 'rb') as img:
+            await message.answer_photo(img)
+    await message.answer(text="Выберите продукт для покупки:", reply_markup=inl_kb_buy)
+
+@dp.callback_query_handler(text = 'product_buying')
+async def send_confirm_message(call):
+    await call.answer()
+    await call.message.answer(text = 'Вы успешно приобрели продукт!')
+
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+    balance = State()
+
+@dp.message_handler(text = 'Регистрация')
+async def sing_up(message: types.Message):
+    await message.answer(text='Введите имя пользователя (только латинский алфавит):')
+    await RegistrationState.username.set()
+
+@dp.message_handler(state = RegistrationState.username)
+async def set_username(message: types.Message, state):
+    if await is_included(message.text) is False:
+        await state.update_data(username = message.text)
+        await message.answer(text='Введите свой email:')
+        await RegistrationState.email.set()
+    else:
+        await message.answer(text='Пользователь существует, введите другое имя')
+        await RegistrationState.username.set()
+
+@dp.message_handler(state = RegistrationState.email)
+async def set_email(message: types.Message, state):
+    await state.update_data(email = message.text)
+    await message.answer(text='Введите свой возраст:')
+    await RegistrationState.age.set()
+
+@dp.message_handler(state = RegistrationState.age)
+async def set_age(message: types.Message, state):
+    await state.update_data(age = message.text)
+    data = await state.get_data()
+    await add_user(data['username'], data['email'], data['age'])
+    await state.finish()
+
+@dp.message_handler()
+async def all_massages(message: types.Message):
+    await message.answer(text='Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
